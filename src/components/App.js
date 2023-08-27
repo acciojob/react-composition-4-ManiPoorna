c04574fae122dd09628acb6ae359d2738a751667
Mani Poorna <ndmprao2001@gmail.com>
2023-08-27 09:41:39 +0530

    V 1.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [user,setUser] = useState({
+    name: "",
+    email:"",
+    password:""
+  })
+  const displayUser = (e)=>{
+    e.preventDefault();
+    console.log(user);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <form onSubmit={(e)=>{displayUser(e)}}>
+          <label>Name</label>
+          <input type="text" onChange={(e)=>setUser({...user,name:e.target.value})} />
+          <label>Email</label>
+          <input type="email" onChange={(e)=>setUser({...user,email:e.target.value})} />
+          <label>Password</label>
+          <input type="password" onChange={(e)=>setUser({...user,password:e.target.value})} />
+          <button type="submit">Submit</button>
+        </form>
     </div>
   )
 }
